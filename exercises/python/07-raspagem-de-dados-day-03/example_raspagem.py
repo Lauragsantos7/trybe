d0bea8bf8330ac4721c933f9aeccc388b6cc8ed8
Laura <lauragsantos7@gmail.com>
2023-08-03 18:10:04 -0300

    configurando e usando mongo DB

---
+++ exercises/python/07-raspagem-de-dados-day-03/example_raspagem.py
@@ -0,0 +1,53 @@
+from pymongo import MongoClient
+
+client = MongoClient()
+db = client.catalogue
+# book representa um dado obtido na raspagem
+book = {
+    "title": "A Light in the Attic",
+}
+document_id = db.books.insert_one(book).inserted_id
+print(document_id)
+client.close()  # fecha a conexão com o banco de dados
+
+
+# inserir multiplos docs:
+from pymongo import MongoClient
+
+client = MongoClient()
+db = client.catalogue
+documents = [
+    {
+        "title": "A Light in the Attic",
+    },
+    {
+        "title": "Tipping the Velvet",
+    },
+    {
+        "title": "Soumission",
+    },
+]
+db.books.insert_many(documents)
+client.close()  # fecha a conexão com o banco de dados
+
+# buscas pelo método find ou find_one:
+from pymongo import MongoClient
+
+client = MongoClient()
+db = client.catalogue
+# busca um documento da coleção, sem filtros
+print(db.books.find_one())
+# busca utilizando filtros
+for book in db.books.find({"title": {"$regex": "t"}}):
+    print(book["title"])
+client.close()  # fecha a conexão com o banco de dados
+
+
+# usando o contexto with:
+from pymongo import MongoClient
+
+
+with MongoClient() as client:
+    db = client.catalogue
+    for book in db.books.find({"title": {"$regex": "t"}}):
+        print(book["title"])
