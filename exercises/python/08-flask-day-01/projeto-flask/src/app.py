857d6989f3a5527fe8767140e6671932f164ca05
Laura <lauragsantos7@gmail.com>
2023-08-15 14:50:03 -0300

    flask- day1- criando rotas, models, e instanciando buscas no db

---
+++ exercises/python/08-flask-day-01/projeto-flask/src/app.py
@@ -0,0 +1,24 @@
+# rodar o container: docker compose up flask-api
+from flask import Flask
+from controllers.jokes_controller import jokes_controller
+from os import environ
+from waitress import serve
+
+app = Flask(__name__)
+app.register_blueprint(jokes_controller, url_prefix="/jokes")
+
+
+
+def start_server(host: str = "0.0.0.0", port: int = 8000):
+    if environ.get("FLASK_ENV") == "dev":
+        # Servidor de desenvolvimento do Kit Werkzeug
+        return app.run(debug=True, host=host, port=port)
+    else:
+        # Este é o waitress, otimizado para produção
+        serve(app, host=host, port=port)
+
+if __name__ == "__main__":
+    start_server()
+
+# Quando usamos o servidor de desenvolvimento do Flask, o servidor é reiniciado a cada alteração no código. No entanto, quando usamos o Waitress, isso não acontece. Portanto, se você fizer alterações no código, precisará reiniciar o servidor manualmente.
+# Como não configuramos nenhum tipo de log para o Waitress, não veremos nenhuma mensagem de log no terminal. Mas pode ter tranquilidade: o servidor está funcionando perfeitamente.
