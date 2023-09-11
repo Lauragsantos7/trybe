1c171625a69c1bffc8da921460f5465a354d32a0
lauragsantos7 <lauragsantos7@gmail.com>
2023-09-11 14:56:15 -0300

    configuração inicial pronta

---
+++ exercises/python/10-django-02-day-01-autenticacao/README.md
@@ -0,0 +1,15 @@
+Configuração projeto:
+criar amb virtual
+instalar apps necessários:
+
+ip install django djangorestframework
+pip install markdown django-filter mysqlclient
+
+* iniciar projeto:
+django-admin startproject marryme .
+
+* Criar um app no projeto chamado budget
+django-admin startapp budget
+
+laura
+513416003Ls
---
+++ exercises/python/10-django-02-day-01-autenticacao/budget/views.py
@@ -0,0 +1,21 @@
+# budget/views.py
+
+
+from rest_framework import viewsets
+from .models import Vendor, Marriage, Budget
+from .serializers import VendorSerializer, MarriageSerializer, BudgetSerializer
+
+
+class VendorViewSet(viewsets.ModelViewSet):
+    queryset = Vendor.objects.all()
+    serializer_class = VendorSerializer
+
+
+class BudgetViewSet(viewsets.ModelViewSet):
+    queryset = Budget.objects.all()
+    serializer_class = BudgetSerializer
+
+
+class MarriageViewSet(viewsets.ModelViewSet):
+    queryset = Marriage.objects.all()
+    serializer_class = MarriageSerializer
