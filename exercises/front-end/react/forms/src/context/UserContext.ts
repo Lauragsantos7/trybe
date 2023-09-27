6399afe1b2e87fa465e38ab4039b56dadb3cb7ca
lauragsantos7 <lauragsantos7@gmail.com>
2023-09-27 17:38:44 -0300

    tipagem do context

--- exercises/front-end/react/forms/src/context/UserContext.ts
+++ exercises/front-end/react/forms/src/context/UserContext.ts
@@ -1,6 +1,11 @@
 import { createContext } from 'react';
+type UserContextType = {
+    nome: string,
+    email: string,
+    cidade: string,
+    bio: string
+}
 
-
-const UserContext = createContext({});
+const UserContext = createContext({} as UserContextType);
 
 export default UserContext;
